e6040c7b63a2ea0a11ca50ad0c0a1f3ff1dd7c56
Real-Master-of-Chaos <paragdhruvincoc@gmail.com>
2020-11-02 21:22:59 +0530

    big commit

--- src/command(s)/stats/bsstats.js
+++ src/command(s)/stats/bsstats.js
@@ -5,6 +5,7 @@ const { oneLine, stripIndent } = require('common-tags');
 const User = require('../../schema(s)/user');
 const fetch = require('node-fetch');
 const config = require('../../../config.json');
+const league = require('../../utils/trophiesmap.json')
 
 module.exports = class BsStatsCommand extends Command {
   constructor(client) {
@@ -17,7 +18,7 @@ module.exports = class BsStatsCommand extends Command {
         Provide no prefix to set the default
       `,
       type: client.types.SETUP,
-      userPermissions: ['MANAGE_GUILD'],
+      userPermissions: ['SEND_MESSAGES'],
       examples: ['bsstats']
     });
   }
@@ -40,32 +41,40 @@ module.exports = class BsStatsCommand extends Command {
     if (tag.startsWith('#')) tag = tag.slice(1);
 
     try {
-        const res = await fetch(`https://api.brawlstars.com/v1/players/%23${tag}`, { headers: {'Authorization': `Bearer ${token}`}});
+        const res = await fetch(`https://bsproxy.royaleapi.dev/v1/players/%23${tag}`, { headers: {'Authorization': `Bearer ${token}`}});
         const stats = await res.json();
         if (!stats.reason) {
 
         let color = stats.nameColor.slice(4)
         color = '#' + color
 
+        let totalwins = stats.soloVictories + stats.duoVictories + stats["3vs3Victories"];
+
+        /*//Calculating needed Exp.
+        let a = 40;
+        let d = 10;
+        let T = a + stats.expLevel * d;
+        let wtf = stats.expLevel / 2      
+        let s = wtf * (2 * a + (stats.expLevel - 1) * d)
+        let curr = s - T
+            console.log(stats.expPoints)*/
+
+            let images;
+            const entries = Object.entries(league);
+            entries.forEach(entry => {
+              // entry[0] is the key, entry[1] is the value.
+              if(stats.trophies > entry[0]) images = entry[1];
+            });
+
         const embed = new MessageEmbed()
-          .setTitle(`${stats.name}'s stats`)
-          //.setImage(img)
+          .setTitle(`${emojis.bs}${stats.name}'s Brawl Stars stats`)
+          .setThumbnail(images)
           .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
-          .setTimestamp()
-          .setDescription(`
-          **Tag:** \`${stats.tag}\`
-          **Experience Level:** \`${stats.expLevel}\`
-          **Current Expereince:** \`${stats.expPoints}\`
-          **Current Trophies:** \`${stats.trophies}\`${emojis.Trophy}
-          **Highest Trophies:** \`${stats.highestTrophies}\`${emojis.Trophy}
-          **Solo Victories:** \`${stats.soloVictories}\`
-          **Duo Victories:** \`${stats.duoVictories}\`
-          **3vs3 Victories:** \`${stats["3vs3Victories"]}\`
-          **Power Play Points:** \`${stats.powerPlayPoints}\`
-          **Highest Power Play Points:** \`${stats.highestPowerPlayPoints}\`
-          **Brawlers:** \`${stats.brawlers.length}\`
-          `)
-          .addField(`Club:`, `**Club Name**: \`${stats.club.name}\`\n**Club Tag**: \`${stats.club.tag}\``)
+          .setTimestamp()//\nExperience: \`${curr}/${T}\`
+          .addField(`Profile Stats:`, `Player Tag: **\`${stats.tag}\`**\nExperience Level: ${emojis.Experience}**\`${stats.expLevel}\`**\nCurrent Trophies: ${emojis.Trophy}**\`${stats.trophies}\`**\nPower Play Points: ${emojis.PowerPlayPoints}**\`${stats.powerPlayPoints}\`**\nUnlocked Brawlers: ${emojis.Unlocked}**\`${stats.brawlers.length}\`**\n`, true)
+          .addField(`Club:`, `Club Name: **\`${stats.club.name}\`**\nClub Tag: **\`${stats.club.tag}\`**\n**Highest's**\nHighest Trophies: ${emojis.Trophy}**\`${stats.highestTrophies}\`**\nHighest in Power Play: ${emojis.PowerPlayPoints}**\`${stats.highestPowerPlayPoints}\`**`, true)
+          .addField(`\u200B`, `\u200B`)
+          .addField(`Victories:`, `Total Victories: **\`${totalwins}\`**\nSolo Victories: ${emojis.Solo}**\`${stats.soloVictories}\`**\nDuo Victories: ${emojis.Duo}**\`${stats.duoVictories}\`**\n3vs3 Victories: ${emojis["3v3"]}**\`${stats["3vs3Victories"]}\`**\n`, true)
           .setColor(color);
 
         message.channel.send(embed);
@@ -75,7 +84,7 @@ module.exports = class BsStatsCommand extends Command {
             message.channel.send(nofind)
         } else {
             const shit = new MessageEmbed()
-            .setDescription(`**Error**: \`${stas.reason}\`\n**Message**: \`${stats.message || 'None'}\``)
+            .setDescription(`**Error:** \`${stats.reason}\`\n**Message:** \`${stats.message || 'None'}\``)
             message.channel.send(shit)
         }
       } catch (err) {
@@ -85,7 +94,7 @@ module.exports = class BsStatsCommand extends Command {
 
     } else if (user.bstag) {
 
-        let tag = user.bstag
+    let tag = user.bstag
 
     const rgx = /^#?[0-9A-Z]/i;
 
@@ -96,42 +105,41 @@ module.exports = class BsStatsCommand extends Command {
     if (tag.startsWith('#')) tag = tag.slice(1);
 
     try {
-        const res = await fetch(`https://api.brawlstars.com/v1/players/%23${tag}`, { headers: {'Authorization': `Bearer ${token}`}});
+        const res = await fetch(`https://bsproxy.royaleapi.dev/v1/players/%23${tag}`, { headers: {'Authorization': `Bearer ${token}`}});
         const stats = await res.json();
         if (!stats.reason) {
 
         let color = stats.nameColor.slice(4)
         color = '#' + color
 
-        const embed = new MessageEmbed()
-          .setTitle(`${stats.name}'s stats`)
-          //.setImage(img)
-          .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
-          .setTimestamp()
-          .setDescription(`
-          **Tag:** \`${stats.tag}\`
-          **Experience Level:** \`${stats.expLevel}\`
-          **Current Expereince:** \`${stats.expPoints}\`
-          **Current Trophies:** \`${stats.trophies}\`${emojis.Trophy}
-          **Highest Trophies:** \`${stats.highestTrophies}\`${emojis.Trophy}
-          **Solo Victories:** \`${stats.soloVictories}\`
-          **Duo Victories:** \`${stats.duoVictories}\`
-          **3vs3 Victories:** \`${stats["3vs3Victories"]}\`
-          **Power Play Points:** \`${stats.powerPlayPoints}\`
-          **Highest Power Play Points:** \`${stats.highestPowerPlayPoints}\`
-          **Brawlers:** \`${stats.brawlers.length}\`
-          `)
-          .addField(`Club:`, `**Club Name**: \`${stats.club.name}\`\n**Club Tag**: \`${stats.club.tag}\``)
-          .setColor(color);
+        let totalwins = stats.soloVictories + stats.duoVictories + stats["3vs3Victories"];
 
-        message.channel.send(embed);
+        let images;
+            const entries = Object.entries(league);
+            entries.forEach(entry => {
+              // entry[0] is the key, entry[1] is the value.
+              if(stats.trophies > entry[0]) images = entry[1];
+            });
+
+        const urembed = new MessageEmbed()
+        .setTitle(`${emojis.bs}${stats.name}'s Brawl Stars stats`)
+        .setThumbnail(images)
+        .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+        .setTimestamp()//\nExperience: \`${curr}/${T}\`
+        .addField(`Profile Stats:`, `Player Tag: **\`${stats.tag}\`**\nExperience Level: ${emojis.Experience}**\`${stats.expLevel}\`**\nCurrent Trophies: ${emojis.Trophy}**\`${stats.trophies}\`**\nPower Play Points: ${emojis.PowerPlayPoints}**\`${stats.powerPlayPoints}\`**\nUnlocked Brawlers: ${emojis.Unlocked}**\`${stats.brawlers.length}\`**\n`, true)
+        .addField(`Club:`, `Club Name: **\`${stats.club.name}\`**\nClub Tag: **\`${stats.club.tag}\`**\n**Highest's**\nHighest Trophies: ${emojis.Trophy}**\`${stats.highestTrophies}\`**\nHighest in Power Play: ${emojis.PowerPlayPoints}**\`${stats.highestPowerPlayPoints}\`**`, true)
+        .addField(`\u200B`, `\u200B`)
+        .addField(`Victories:`, `Total Victories: **\`${totalwins}\`**\nSolo Victories: ${emojis.Solo}**\`${stats.soloVictories}\`**\nDuo Victories: ${emojis.Duo}**\`${stats.duoVictories}\`**\n3vs3 Victories: ${emojis["3v3"]}**\`${stats["3vs3Victories"]}\`**\n`, true)
+        .setColor(color);
+
+        return message.channel.send(urembed);
         } else if (stats.reason === 'notFound') {
             const nofind = new MessageEmbed()
             .setDescription(`${emojis.Sad} The tag linked to your account is Invalid.`)
             message.channel.send(nofind)
         } else {
             const shit = new MessageEmbed()
-            .setDescription(`**Error**: \`${stas.reason}\`\n**Message**: \`${stats.message || 'None'}\``)
+            .setDescription(`Error: \`${stats.reason}\`\nMessage: \`${stats.message || 'None'}\``)
         }
       } catch (err) {
         message.client.logger.error(err.stack);
